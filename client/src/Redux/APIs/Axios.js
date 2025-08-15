ec985c872fe4775e8a0a386d055badaf2ff08e2d
ThuanPhat-design <phattran09082004@gmail.com>
2025-08-15 13:03:54 +0700

    Fix CORS for GitHub Pages 2

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -1,11 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const Axios = axios.create({
-  baseURL:
-    process.env.NODE_ENV === 'production'
-      ? 'https://webxemphim-devops.vercel.app/api'
-      : 'http://localhost:5000/api',
-  withCredentials: true
+const API = axios.create({
+  baseURL: "https://webxemphim-devops.vercel.app/api", // Đúng domain backend
+  withCredentials: true, // nếu có dùng cookie/session
 });
 
-export default Axios;
+export default API;
