db04c5d8da4e87b7393a84763efede64f3e5506f
ThuanPhat-design <phattran09082004@gmail.com>
2025-08-15 21:18:33 +0700

    test ployy 1

--- client/src/Redux/APIs/Axios.js
+++ client/src/Redux/APIs/Axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_URL =
   process.env.NODE_ENV === "production"
-    ? "https://webxemphim-devops.vercel.app/api" // domain cố định backend
+    ? "https://webxemphim-devops-jv4zm8kx7-thuanphat-designs-projects.vercel.app" // domain cố định backend
     : "http://localhost:5000/api";
 
 const Axios = axios.create({
